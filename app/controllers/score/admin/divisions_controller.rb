c6e703978a3953b4025d64b6ff850ec2c204f2cc
Chris Woodall <cmwoodall@game7.net>
2011-11-30 13:13:52 -0700

    crudify admin/divisions

--- app/controllers/score/admin/divisions_controller.rb
+++ app/controllers/score/admin/divisions_controller.rb
@@ -1,9 +1,10 @@
 module Score
   module Admin
     class DivisionsController < ApplicationController
+      
+      crudify 'score/division'
 
-      before_filter :find_division, :only => [:edit, :update, :destroy]
-      before_filter :find_season, :only => [:index, :new, :create, :edit, :update, :destroy]
+      before_filter :find_season
       
       def index
         @divisions = @season.divisions.asc(:name)
@@ -12,41 +13,16 @@ module Score
           format.html
         end    
       end
-
-      def new
-        @division = Score::Division.new
-      end
-
-      def create
-        @division = @season.divisions.build(params[:division])
-        if @division.save
-          flash[:notice] = "New Division has been Created"
-        end
-      end
-
-      def edit
-
-      end
-
-      def update
-        if @division.update_attributes(params[:division])
-          flash[:notice] = "Division has been Updated"
-        end
-      end
-
-      def destroy
-        @division.destroy
-        flash[:notice] = "Division has been deleted"
+      
+      def before_create
+        @instance = @division = @season.divisions.build(params[:division])
+        true
       end
 
       private
       
         def find_season()
           @season = @division ? @division.season : Score::Season.find(params[:season_id])
-        end
-
-        def find_division()
-          @division = Score::Division.find(params[:id])
         end
 
     end
